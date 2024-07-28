5e4ad87681bd5e6a0479586101adb41d9258d74b
Adrian Alejandro Cruz Alanis <alejandrocruzalanis@gmail.com>
2024-07-27 20:34:31 -0600

    Creation of a frontend login page
    This page will be the first window the user is going to look at when opening the application
    
    
    fixes #2

--- index.py
+++ index.py
@@ -6,4 +6,24 @@ window.title("AquaSport")
 
 window.geometry("1280x720")
 
+label_user = tk.Label(window, text="User:")
+
+entry_user = tk.Entry(window)
+
+label_password = tk.Label(window, text="Password:")
+
+entry_password = tk.Entry(window, show="*")
+
+login_btn = tk.Button(window, text="Login", command=set)
+
+label_user.pack()
+
+entry_user.pack()
+
+label_password.pack()
+
+entry_password.pack()
+
+login_btn.pack()
+
 window.mainloop()
